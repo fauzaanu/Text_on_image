c8f9fee6bbcbc3937092d79c72fe4d3f0f2867a8
fauzaanu <fauzaangasim@gmail.com>
2021-08-05 00:01:36 -0400

    done

--- gen.py
+++ gen.py
@@ -2,21 +2,27 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-def textOnImg(text,color,tcolor):
+def textOnImg(text,color,tcolor,padding=20,font_name="Roboto-Light.ttf",font_size = 25,adjuster=.5,save_as="overlay.jpg",fk_img_dim=1080):
+    """
+    ...
+    """
     #to calculate the what the width of the image should be based on the text
-    fake_img = Image.new('RGB',(1080,1080),'white')
-    fontObj = ImageFont.truetype("Roboto-Light.ttf",25)
+    fake_img = Image.new('RGB',(fk_img_dim,fk_img_dim),'white')
+    fontObj = ImageFont.truetype("Roboto-Light.ttf",font_size)
     fake_draw = ImageDraw.Draw(fake_img)
     BG_W,BG_H = fake_draw.textsize(text,fontObj) #get the text dimensions to use on the real img
 
     #real image...
-    real_img = Image.new('RGB',(BG_W+20,BG_H+20),color)
+    add_to_w = padding
+    add_to_h = padding #two vars incase i need to customize this on another occasion...
+    real_img = Image.new('RGB',(BG_W+add_to_w,BG_H+add_to_h),color)
     real_draw = ImageDraw.Draw(real_img)
-    fontObj = ImageFont.truetype("Roboto-Light.ttf",25)
+    fontObj = ImageFont.truetype(font_name,font_size)
 
     #add text + save
-    real_draw.text((10,10), text, tcolor, font=fontObj)
-    real_img.save('overlay.jpg')
+    adjust = adjuster #sometimes when the text is not vertically centered some adjustment is needed...(same happens on illustrator unless we make the text an outline)
+    real_draw.text((add_to_w/2,add_to_h/2+adjust), text, tcolor, font=fontObj)
+    real_img.save(save_as)
 
 
 
@@ -27,5 +33,3 @@ if __name__ == "__main__":
     tcolor = input("Enter Text Color: ")
 
     textOnImg(text,color,tcolor)
-
-    
